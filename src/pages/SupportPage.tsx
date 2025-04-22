425ce1622ccbb159bb89b5719b99789d7a4bbb87
Ireme-Nyawe <120557087+Ireme-Nyawe@users.noreply.github.com>
2025-04-22 23:31:30 +0200

    Update SupportPage

--- src/pages/SupportPage.tsx
+++ src/pages/SupportPage.tsx
@@ -139,12 +139,18 @@ const SupportPage: React.FC<any> = () => {
   const createPeerConnection = (techId: string) => {
     console.log("Creating peer connection to connect with technician:", techId);
 
-    const configuration = {
-      iceServers: [
-        { urls: "stun:stun.l.google.com:19302" },
-        { urls: "stun:stun1.l.google.com:19302" },
-      ],
-    };
+      const configuration = {
+        iceServers: [
+          { urls: 'stun:stun.l.google.com:19302' },
+          {
+            urls: 'turn:openrelay.metered.ca:80',
+            username: 'openrelayproject',
+            credential: 'openrelayproject'
+          }
+        ],
+        sdpSemantics: 'unified-plan'
+      };
+
 
     try {
       const pc = new RTCPeerConnection(configuration);
