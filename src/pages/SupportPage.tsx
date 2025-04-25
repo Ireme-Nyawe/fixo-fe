cdcdd1da4f4993bf15cabdf48e795b11ed85b32e
Ireme-Nyawe <akimana.inono@gmail.com>
2025-04-25 11:57:08 +0200

    add twilio TURN and STURN server

--- src/pages/SupportPage.tsx
+++ src/pages/SupportPage.tsx
@@ -140,15 +140,11 @@ const SupportPage: React.FC<any> = () => {
   
     const configuration = {
       iceServers: [
-        { urls: "stun:stun.l.google.com:19302" },
-        { urls: "stun:stun1.l.google.com:19302" },
-        { urls: "stun:stun2.l.google.com:19302" },
-        { urls: "stun:stun3.l.google.com:19302" },
-        { urls: "stun:stun4.l.google.com:19302" },
+        { urls: "stun:global.stun.twilio.com:3478" },
         {
-          urls: "turn:openrelay.metered.ca:443",
-          username: "openrelayproject",
-          credential: "openrelayproject",
+          urls: "turn:global.turn.twilio.com:3478?transport=udp",
+          username: "dc2d2894d5a9023620c467b0e71cfa6a35457e6679785ed6ae9856fe5bdfa269",
+          credential: "tE2DajzSJwnsSbc123",
         },
       ],
       iceCandidatePoolSize: 10,
