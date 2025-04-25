18ba4df58bd923315aa995df6e8ed0aecb48ef00
Ireme-Nyawe <akimana.inono@gmail.com>
2025-04-25 14:56:37 +0200

    fixx twilio server

--- src/components/dashboard/TechnicianCallView.tsx
+++ src/components/dashboard/TechnicianCallView.tsx
@@ -119,17 +119,21 @@ const TechnicianCallView: React.FC<TechnicianCallViewProps> = ({
   
     const configuration = {
       iceServers: [
-        { urls: "stun:global.stun.twilio.com:3478" },
+        { urls: "stun:stun.l.google.com:19302" },
+        { urls: "stun:stun1.l.google.com:19302" },
+        { urls: "stun:stun2.l.google.com:19302" },
+        { urls: "stun:stun3.l.google.com:19302" },
+        { urls: "stun:stun4.l.google.com:19302" },
         {
           urls: "turn:global.turn.twilio.com:3478?transport=udp",
           username: "dc2d2894d5a9023620c467b0e71cfa6a35457e6679785ed6ae9856fe5bdfa269",
-          credential: "tE2DajzSJwnsSbc123",
-        },
+          credential: "tE2DajzSJwnsSbc123"
+        }
       ],
       iceCandidatePoolSize: 10,
-      sdpSemantics: "unified-plan",
+      sdpSemantics: "unified-plan"
     };
-  
+    
     try {
       const pc = new RTCPeerConnection(configuration);
       peerConnection.current = pc;
