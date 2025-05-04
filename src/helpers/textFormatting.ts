59b76960eee38f5f0f92e506a052d691160c90d2
Ndahimana Bonheur <ndahimana154@gmail.com>
2025-05-05 03:13:19 +0800

    Tech view balances

--- src/helpers/textFormatting.ts
+++ src/helpers/textFormatting.ts
@@ -1,4 +1,17 @@
 export const truncateText = (text: string | undefined, length: number): string => {
-    if (!text) return '';
-    return text.length > length ? `${text.slice(0, length)}...` : text;
+  if (!text) return '';
+  return text.length > length ? `${text.slice(0, length)}...` : text;
+};
+
+
+export const formatCurrency = (amount: number): string => {
+  if (amount >= 1000000) {
+    const millions = (amount / 1000000).toFixed(1);
+    return `${millions.replace(/\.0$/, '')}M RWF`;
+  }
+  if (amount >= 1000) {
+    const thousands = (amount / 1000).toFixed(1);
+    return `${thousands.replace(/\.0$/, '')}K RWF`;
+  }
+  return `${amount} RWF`;
 };
--- src/pages/dashboard/DashboardLayout.tsx
+++ src/pages/dashboard/DashboardLayout.tsx
@@ -1,19 +1,35 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import DashboardHeader from '../../components/dashboard/DashboardHeader';
 import DashboardSidebar from '../../components/dashboard/DashboardSidebar';
 import { Toaster } from 'sonner';
+import authService from '../../state/features/auth/authService';
 
 const DashboardLayout = () => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-
+  const [profile, setProfile] = useState<any>(null);
   const toggleSidebar = () => {
     setIsSidebarOpen(!isSidebarOpen);
+  };
+  useEffect(() => {
+    findProfile();
+  }, []);
+
+  const findProfile = async () => {
+    try {
+      const response = await authService.getProfile();
+      if (response.status === 200) {
+        localStorage.setItem('profile', JSON.stringify(response.data));
+        setProfile(response.data);
+      } else {
+        console.error('Error fetching profile:', response.message);
+      }
+    } catch (error) {}
   };
   return (
     <div className="h-screen flex flex-col bg-primary overflow-hidden">
       <Toaster richColors position="top-center" />
-      <DashboardHeader sideBarToggle={toggleSidebar} />
+      <DashboardHeader sideBarToggle={toggleSidebar} profile={profile} />
 
       <div className="flex flex-1 overflow-hidden">
         <DashboardSidebar
