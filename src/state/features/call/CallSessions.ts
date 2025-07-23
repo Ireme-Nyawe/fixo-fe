0a7569c43813bd11d79b29783908e8f7c2db2d6b
Ireme-Nyawe <akimana.inono@gmail.com>
2025-07-23 09:33:09 +0200

    fetch sessions

---
+++ src/state/features/call/CallSessions.ts
@@ -0,0 +1,13 @@
+import { axiosInstance } from "../../../utils/axios";
+import { handleError } from "../auth/authService";
+
+export const getCallsessionsByDateRange = async (start:any,end:any) => {
+    try {
+        const response = await axiosInstance.get(`/api/call/session-range?start=${start}&end=${end}`);
+        console.log(response);
+        
+        return response.data;
+    } catch (error) {
+        return handleError(error);
+    }
+}
--- src/types/store.d.ts
+++ src/types/store.d.ts
@@ -77,3 +77,4 @@ export interface Transaction {
   description: string;
   createdAt: string;
 }
+
